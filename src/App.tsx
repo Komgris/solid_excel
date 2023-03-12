91e816185b9defa0b5831509943cf9659826c781
DESKTOP-OJSJDPM\komgrisr <komgris.psec@gmail.om>
2023-03-12 16:38:58 +0700

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+    <div class="container mx-auto">
+      Hello world!
+    </div>
+  );
+};
+
+export default App;
